8580cf14882fdf662084c77987cba4fd7c09b165
MatthewRafei <matthewrafei1@gmail.com>
2026-01-29 15:28:24 -0600

    Add start file

---
+++ pinky.py
@@ -0,0 +1,12 @@
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        raise SystemExit('Usage: python3 pinky.py <filename>')
+    filename = sys.argv[1]
+    print("FileName: " + str(filename))
+    
+    with open(filename) as file:
+        source = file.read()
+        print(source)
+        
