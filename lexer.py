de54a64f33f3d89a6ef979fd680b8b60989d5315
MatthewRafei <matthewrafei1@gmail.com>
2026-02-02 16:14:28 -0600

    Fix lexer bug and add more types

--- lexer.py
+++ lexer.py
@@ -1,18 +1,22 @@
 from tokens import *
 
 class Lexer:
-    def __int__(source):
+    def __init__(self, source):
         self.source = source
         self.start = 0
         self.current = 0
         self.line = 0
         self.column = 0
         self.tokens = []
+        pass
 
     def advance(self):
-        character = self.source[self.curr]
+        character = self.source[self.current]
         self.current = self.current + 1
         return character
+    
+    def add_token(self, token_type):
+        self.tokens.append(Token(token_type, self.source[self.start:self.current]))
 
     def tokenize(self):
         while self.current < len(self.source):
@@ -20,6 +24,10 @@ class Lexer:
             character = self.advance()
             
             if character == "+":
-                self.tokens.append(Token(TOK_PLUS, self.source[self.start:self.current]))
+                self.add_token(TOK_PLUS)
+            elif character == "-":
+                self.add_token(TOK_MINUS)
+            elif isinstance(character, int):
+                self.add_token(TOK_INTEGER)
             
         return self.tokens
